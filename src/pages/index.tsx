f658348bb61d3ca6ae80aecc99e72e720d6f81ab
MarcelFrontend <marceltomasz1806@gmail.com>
2024-10-02 22:52:18 +0200

    początek budowy aplikacji

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,95 @@
-import Image from "next/image";
-import localFont from "next/font/local";
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import { GoArrowLeft } from "react-icons/go";
+import { GoGear } from "react-icons/go";
+import { GoPlus } from "react-icons/go";
+export default function Index() {
+  const months = [
+    "Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec",
+    "Lipiec", "Sierpień", "Wrzesień", "Pażdziernik", "Listopad", "Grudzień"
+  ];
+  const [expenses, setExpenses] = useState<number[]>([]);
+  const [dailyExpenses, setDailyExpenses] = useState<number[]>([]);
+  const [showMonthExpenses, SetShowMonthExpenses] = useState(false)
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+  const generateMonthExpense = () => {
+    let expense;
+    do {
+      expense = Math.floor(Math.random() * 1000);
+    } while (expense > 150 || expense < 50);
+    return expense;
+  };
+  const generateDayExpense = () => {
+    let expense;
+    do {
+      expense = Math.floor(Math.random() * 1000);
+    } while (expense > 60 || expense < 8);
+    return expense;
+  };
+  useEffect(() => {
+    const generatedExpenses = months.map(generateMonthExpense);
+    const generatedDailyExpenses = Array.from({ length: daysInCurrentMonth }, generateDayExpense);
+    setDailyExpenses(generatedDailyExpenses);
 
-export default function Home() {
+    setExpenses(generatedExpenses);
+  }, []);
+
+  const showMonthDetails = () => { }
+  const addNewExpenseInMonths = () => { }
+
+  function getDaysInMonth(year: number, month: number) {
+    return new Date(year, month, 0).getDate();
+  }
+
+  const date = new Date();
+  const currentYear = date.getFullYear();
+  const currentMonth = date.getMonth() + 1;
+  const daysInCurrentMonth = getDaysInMonth(
+    currentYear,
+    currentMonth,
+  );
+
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <Head>
+        <title>Expense Tracker</title>
+      </Head>
+      <div className="relative h-screen flex flex-col overflow-y-hidden">
+        <header className="w-screen h-10 flex items-center justify-between px-2">
+          <GoArrowLeft className="w-8 h-8" />
+          <span className="sticky top-1 text-2xl p-2 text-green-500 font-bold">2024</span>
+          <GoGear className="w-7 h-7" />
+        </header>
+        <main className="relative flex-grow flex items-center justify-center flex-col gap-2 overflow-hidden">
+          <div className="w-screen">
+            {!showMonthExpenses ? (
+              <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 pb-4 px-2 md:px-0">
+                {months.map((month, index) => (
+                  <li onClick={() => { showMonthDetails(); SetShowMonthExpenses(!showMonthExpenses) }} key={month} className="min-w-36 flex items-center justify-between gap-2 px-2 py-1 text-lg lg:text-3xl
+              border-2 border-green-500 rounded-md cursor-pointer hover:scale-105 transition-transform active:scale-95">
+                    <span>{month}</span>
+                    <span className={`${expenses[index] > 120 ? "text-red-500 font-black"
+                      : expenses[index] > 80 ? "text-yellow-400" : "text-green-500"} tracking-wide`}>
+                      {expenses[index] + "zł" || "Błąd"}</span>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <ul className="max-w-[35rem] mx-auto grid grid-cols-7 select-none">
+                {dailyExpenses.map((expense, day) => (
+                  <li key={day} className={`w-20 h-20 border ${expense > 50 ? "bg-red-400 font-black" : expense > 30 ? "bg-yellow-400/95" : "bg-green-500"} tracking-wide text-black hover:opacity-90 active:opacity-75 transition-opacity cursor-pointer flex items-center justify-center`}>  
+                    {expense}zł
+                  </li>
+                ))}
+              </ul>
+            )
+            }
+          </div>
+        </main>
+        <button onClick={addNewExpenseInMonths} className="w-[calc(100vw-25px)] md:w-[calc(100vw-50px)] flex items-center justify-center mx-3 mb-3 rounded-full bg-green-500 hover:scale-105 active:scale-[0.98] transition-transform">
+          <GoPlus className="w-10 h-10" />
+        </button>
+      </div>
     </div>
   );
 }
