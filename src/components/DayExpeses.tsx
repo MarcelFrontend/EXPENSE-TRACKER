15e152d329ccc952446d0a90a76030aa2c55def3
MarcelFrontend <marceltomasz1806@gmail.com>
2024-10-15 17:33:16 +0200

    Naprawienie responsywności oraz dodanie inputa z nazwą sklepu

--- src/components/DayExpeses.tsx
+++ src/components/DayExpeses.tsx
@@ -109,7 +109,6 @@ export default function DayExpeses({ setSelectedDayData, selectedDayData, setSel
         }
     }
 
-
     function getBack() {
         const updatedData = { ...storedData };
         setStoredData(updatedData);
